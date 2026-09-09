774893034d1d3ff504c7254ede9af3076dff00b1
kruthibabu7204-rgb <kruthibabu7204@gmail.com>
2026-09-09 10:26:00 +0530

    Add files via upload

---
+++ contineu example.java
@@ -0,0 +1,11 @@
+public class breakexample {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+
+            if (i == 6) {
+                continue;
+            }
+        }
+    }
+}
